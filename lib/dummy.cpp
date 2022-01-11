a61f71710cf87338ccf5a310e9647e79c5b19daf
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2022-01-11 13:20:15 +0000

    remove debugging

--- lib/dummy.cpp
+++ lib/dummy.cpp
@@ -5,7 +5,7 @@ namespace fs = std::filesystem;
 int main(int argc, char* argv[]) {
   if (argc > 1) {
     auto stem = fs::absolute(fs::path{argv[1]}).parent_path() / fs::absolute(fs::path{argv[1]}).stem();
-    std::cout << "stem " << stem << std::endl;
+//    std::cout << "stem " << stem << std::endl;
     std::ofstream(stem.string() + ".out") << "dummy" << std::endl;
     std::ofstream(stem.string() + ".xml") << "<?xml version=\"1.0\"?>\n<root/>" << std::endl;
   }

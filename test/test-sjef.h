8166b35bb7863a9653953074bf1dbe0effc681d0
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2022-01-11 11:49:48 +0000

    portable dummy backend run program

--- test/test-sjef.h
+++ test/test-sjef.h
@@ -8,6 +8,16 @@
 
 #ifdef WIN32
 const std::string path_environment_separator = ";";
+inline int setenv(const char *name, const char *value, int overwrite)
+{
+  int errcode = 0;
+  if(!overwrite) {
+    size_t envsize = 0;
+    errcode = getenv_s(&envsize, NULL, 0, name);
+    if(errcode || envsize) return errcode;
+  }
+  return _putenv_s(name, value);
+}
 #else
 const std::string path_environment_separator = ":";
 #endif

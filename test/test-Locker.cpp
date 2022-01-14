3427fa069ceaa3a8902692de2033c801ba543e21
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2022-01-14 14:57:38 +0000

    Changes by peterk

--- test/test-Locker.cpp
+++ test/test-Locker.cpp
@@ -150,6 +150,8 @@ TEST(Locker, Interprocess) {
   auto lockfile = logfile + ".lock";
   std::filesystem::remove(logfile);
   std::filesystem::remove(lockfile);
+  auto logger = fs::current_path() / (std::string{"logger"} + std::string{EXECUTABLE_SUFFIX});
+//  std::cout << logger << std::endl;
   std::vector<bp::child> processes;
   for (int i = 0; i < 50; ++i) {
     processes.emplace_back(logger.string(), std::vector<std::string>{logfile, "1"});

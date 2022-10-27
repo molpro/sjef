e555438ca33fa27afed323a656c8a3e4fd90c4c6
Nick Wilson <12475993+nick-wilson@users.noreply.github.com>
2022-10-27 17:39:35 +0100

    update bld.bat

--- bld.bat
+++ bld.bat
@@ -1,10 +1,10 @@
 
 set PREFIX=%CONDA_PREFIX%
 
-cmake -LAH -DBUILD_TESTS=OFF -DCMAKE_PREFIX_PATH="%PREFIX%"  -DCMAKE_INSTALL_PREFIX="%PREFIX%"  -DCMAKE_INSTALL_RPATH="%PREFIX%/lib"  -DCMAKE_INSTALL_NAME_DIR="%PREFIX%/lib"  -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON  -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=ON -B cmake-build .
+cmake -LAH -DBUILD_TESTS=OFF -DCMAKE_INSTALL_PREFIX=%PREFIX% -DCMAKE_INSTALL_INCLUDEDIR=%PREFIX%\include  -DCMAKE_INSTALL_LIBDIR=%PREFIX%\Library\lib -B cmake-build .
 
 cmake --build cmake-build --config Release --target install
 
 cd pysjef
-echo __version__ = "0.0.0" > pysjef/_version.py
+echo __version__ = "%1" > pysjef/_version.py
 python -m pip install --no-deps --force-reinstall --verbose .

c8d72e296cda016eddab88e68fdac3c38fd47bfa
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2023-12-05 09:10:29 -0800

    For Windows

--- pysjef/create_conda_env.sh
+++ pysjef/create_conda_env.sh
@@ -4,4 +4,4 @@ if [[ $# -ne 1 ]]; then
 fi
 
 conda config --add channels conda-forge
-conda create --name $1 'python>=3.10' 'numpy>=1.17' 'boost-cpp>=1.71' 'pugixml>=1.10' 'cython=0.29.36' 'lxml>=4.0' pip regex c-compiler cxx-compiler ghostscript
+conda create --name $1 --file requirements.txt

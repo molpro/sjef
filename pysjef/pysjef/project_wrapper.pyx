5e7636ee0909aa09589f87b79951a49554376e5b
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2025-10-20 09:29:08 +0100

    windows CI

--- pysjef/pysjef/project_wrapper.pyx
+++ pysjef/pysjef/project_wrapper.pyx
@@ -1,5 +1,5 @@
 #distutils: language = c++
-#distutils: extra_compile_args = /std=c++17
+# distutils: extra_compile_args =  /std:c++17
 from cython.operator cimport dereference as deref
 from libcpp cimport nullptr, bool
 from libcpp.string cimport string

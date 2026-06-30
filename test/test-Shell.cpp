55310003d3ee26f592a3c3f9e3d2553c505cabb6
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2026-06-30 17:59:50 +0100

    Don't assume /tmp writable in CI

--- test/test-Shell.cpp
+++ test/test-Shell.cpp
@@ -51,22 +51,23 @@ TEST(Shell, remote) {
 TEST(Shell, local_asynchronous) {
   if (sjef::util::Shell::local_asynchronous_supported()) {
     sjef::util::Shell comm;
-    comm("rm -f /tmp/pwd.txt", true);
+    fs::path testfile{"test_local_asynchronous"};
+    comm("rm -f "+testfile.string(), true);
     auto start_time = std::chrono::system_clock::now();
-    comm("sleep 1; touch /tmp/pwd.txt", false);//,"/tmp",4);
+    comm("sleep 1; touch "+testfile.string(), false);//,"/tmp",4);
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - start_time).count();
     EXPECT_LT(duration, 1000) << "Asynchronous shell took too long to return: " << duration << " ms";
     EXPECT_NE(comm.job_number(), 0) << "Output stream:\n"
                                     << comm.out() << std::endl
                                     << "Error stream:\n"
                                     << comm.err() << std::endl;
-    EXPECT_FALSE(fs::exists("/tmp/pwd.txt"));
+    EXPECT_FALSE(fs::exists(testfile.string()));
     comm.wait();
     EXPECT_GT(
         std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - start_time).count(), 1000)
         << "Asynchronous shell took too short time to finish: " << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - start_time).count() << " ms";
     EXPECT_FALSE(comm.running());
-    EXPECT_TRUE(fs::exists("/tmp/pwd.txt"));
+    EXPECT_TRUE(fs::exists(testfile.string()));
   }
 }
 #ifndef WIN32

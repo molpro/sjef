50c2d3abcac01fdfa5d118be9c13da77f93fc846
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2026-07-08 09:11:19 +0100

    Tidy up

--- pysjef/create_conda_env.sh
+++
@@ -1,7 +0,0 @@
-if [[ $# -ne 1 ]]; then
-    echo "Must pass name of environment as arguement"
-    exit 2
-fi
-
-conda config --add channels conda-forge
-conda create --name $1 --file requirements.txt
